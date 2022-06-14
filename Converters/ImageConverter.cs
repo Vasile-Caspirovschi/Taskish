ad1d884065a5a7eee42db5ec7febdc3607f43832
Vasile Caspirovschi <caspirovschi.vasile@elev.cihcahul.md>
2022-06-14 17:23:46 +0300

    Optimized user profile, image showing

---
+++ Converters/ImageConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Taskish.Converters
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return string.Format(@"/Assets/Images/{0}", parameter.ToString());
+            else
+                return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
